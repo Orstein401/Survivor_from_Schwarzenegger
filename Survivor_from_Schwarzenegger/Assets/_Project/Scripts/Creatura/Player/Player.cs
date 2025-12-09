0c9c111013ed6d8418d23fcb3419e5da477317ca
grammer89 <raffaelescotti89@gmail.com>
2025-12-09 19:43:43 +0100

    Add player modify

---
+++ Survivor_from_Schwarzenegger/Assets/_Project/Scripts/Creatura/Player/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    private float _h;
+    private float _v;
+    private Vector2 _position;
+    private Rigidbody2D _rb;
+    [SerializeField] float _speed;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _h = Input.GetAxis("Horizontal");
+        _v = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _position.x = _h;
+        _position.y = _v;
+        if (_position.magnitude > 1)
+        {
+            _position.x = _position.x / Mathf.Sqrt(_position.magnitude);
+            _position.y = _position.y / Mathf.Sqrt(_position.magnitude);
+        }
+        _rb.MovePosition(_rb.position + _position * (_speed * Time.deltaTime));
+    }
+}
